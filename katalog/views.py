d31ab43faf789b47d9bd58767de08eb3cdb0135b
morenoraw <moreno.rassya@ui.ac.id>
2022-09-14 17:50:36 +0700

    Telah menyelesaikan Tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+
+def show_katalog(request):
+    return render(request, "katalog.html", context)
+
+data_barang_catalog = CatalogItem.objects.all()
+context = {
+    'list_barang': data_barang_catalog,
+    'nama': 'Moreno Wibisana',
+    'npm': '2106752003'
+}
